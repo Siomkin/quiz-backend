37431d954f13a4398d5788d7d2af40247ac1d113
Siomkin Alexander <siomkin.alexander@gmail.com>
2019-04-17 12:50:00 +0300

    encore support has been added

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
